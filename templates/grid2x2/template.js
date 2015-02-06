77a212e65762b93072cec35d1694619931928d75
wannabeCitizen <doorsofskirpan@gmail.com>
2015-02-05 20:13:34 -0700

    templates are piping

--- templates/grid2x2/template.js
+++ templates/grid2x2/template.js
@@ -1,30 +1,31 @@
 "use strict";
 
 var rawStream = require("../../lib/stream").rawStream;
-
+var path = require("path");
+var template = path.join(__dirname, "/index.html");
 
 var grid2x2 = {
     g11: function(data) {
         var newData = { "1-1": data };
-        rawStream(newData);
+        rawStream(newData, template);
 
     },
 
     g12: function(data) {
         var newData = { "1-2": data };
-        rawStream(newData);
+        rawStream(newData, template);
 
     },
 
     g21: function(data) {
         var newData = { "2-1": data };
-        rawStream(newData);
+        rawStream(newData, template);
 
     },
 
     g22: function(data) {
         var newData = { "2-2": data };
-        rawStream(newData);
+        rawStream(newData, template);
     }
 };
 
--- templates/leftright/template.js
+++ templates/leftright/template.js
@@ -1,13 +1,19 @@
+"use strict";
+
+var rawStream = require("../../lib/stream").rawStream;
+var path = require("path");
+var template = path.join(__dirname, "/index.html");
+
 var leftright = {
     left: function (data) {
         newData = { "left" : data }
-        return newData;
+        rawStream(newData, template);
 
     },
 
     right: function (data) {
         newData = { "right" : data }
-        return newData;
+        rawStream(newData, template);
 
     }
 
