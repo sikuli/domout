efbf90621a7d2a27ef0103d47f8167042ffe5216
Develra <michael.drew.aaron@gmail.com>
2015-02-05 20:15:41 -0700

    add chalk functionality to .log template

--- templates/log/template.js
+++ templates/log/template.js
@@ -2,11 +2,11 @@
 
 var rawStream = require("../../lib/stream").rawStream;
 var path = require("path");
-
+var ansi_up = require("ansi_up")
 
 var log = function(data) {
      var template = path.join(__dirname, "/index.html");
-     var newData = { "main": data };
+     var newData = { "main": ansi_up.ansi_to_html(data) };
      return rawStream(newData, template);
 };
 
