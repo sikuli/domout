6a725d6861ab209c97cdf4a848fda628f0d895c8
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-02-05 21:16:00 -0700

    Add initial tests

--- templates/log/template.js
+++ templates/log/template.js
@@ -1,13 +1,13 @@
 "use strict";
 
-var rawStream = require("../../lib/stream").rawStream;
+var stream = require("../../lib/stream");
 var path = require("path");
 
 
 var log = function(data) {
      var template = path.join(__dirname, "/index.html");
      var newData = { "main": data };
-     return rawStream(newData, template);
+     return stream.rawStream(newData, template);
 };
 
 
